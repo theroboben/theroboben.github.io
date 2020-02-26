1bcf0d3340b8de521186571e92b6b1e4ebbcd944
jazz-23 <justinfrauenhofer@gmail.com>
2020-02-25 22:49:38 -0800

    updated script

--- script.bat
+++ script.bat
@@ -8,3 +8,7 @@ exit /b
 echo ^(Get-Content "%3"^) ^| ForEach-Object { $_ -replace %1, %2 } ^| Set-Content %4>Rep.ps1
 Powershell.exe -executionpolicy ByPass -File Rep.ps1
 if exist Rep.ps1 del Rep.ps1
+
+git stage index.html
+git commit -m"new code"
+git push
